85dc953b15f6a7e405c875dda79561100d2c8626
Ignacio Aguado <inike2004@gmail.com>
2025-02-27 19:59:39 +0100

    mis commits

--- insert_script.sql
+++ insert_script.sql
@@ -1,64 +1,48 @@
--- AUTHORS
-INSERT INTO authors
-SELECT DISTINCT main_author
-FROM fsdb.acervus
-WHERE main_author IS NOT NULL;
+COLUMN surname FORMAT A30
+COLUMN name FORMAT A30
+COLUMN birth_year FORMAT A30
+COLUMN death_year FORMAT A30
+COLUMN is_author FORMAT A10
 
--- BOOKS
-INSERT INTO books (
-    title, 
-    author_id, 
-    pub_country, 
-    pub_date, 
-    alternative_titles, 
-    subject, 
-    content_notes, 
-    awards,
-    other_authors,
-    mention_authors
-)
 SELECT DISTINCT
-    title,
-    main_author,
-    pub_country,
-    TO_NUMBER(pub_date),
-    alt_title,
-    topic,
-    content_notes,
-    awards,
-    other_authors,
-    mention_authors
+  TRIM(REGEXP_SUBSTR(main_author, '^[^,]+')) AS surname,
+  CASE 
+    WHEN SUBSTR(TRIM(SUBSTR(main_author, INSTR(main_author, ',') + 1)), 1, 1) = '(' 
+      THEN NULL
+    ELSE TRIM(
+           REGEXP_REPLACE(
+             REGEXP_SUBSTR(main_author, ',\s*([^,]+)', 1, 1, NULL, 1),
+             '\( *autor *\)',
+             ''
+           )
+         )
+  END AS name,
+  TRIM(REGEXP_SUBSTR(main_author, '\(\s*(\d{4})', 1, 1, NULL, 1)) AS birth_year,
+  TRIM(REGEXP_SUBSTR(main_author, '-\s*(\d{4})', 1, 1, NULL, 1)) AS death_year,
+  CASE 
+    WHEN REGEXP_LIKE(main_author, '\( *autor *\)') THEN 'author'
+    ELSE NULL
+  END AS role
 FROM fsdb.acervus
-WHERE title IS NOT NULL AND main_author IS NOT NULL;
+FETCH FIRST 50 ROWS only;
 
--- COPIES
-INSERT INTO copies (
-  id,
-  publication_id,
-  comments)
-SELECT
-  signature,
-  isbn,
-  notes
-FROM fsdb.acervus;
-
--- MUNICIPALITIES
+-- Municipalities
 INSERT INTO municipalities (name, province, population, has_library)
-SELECT DISTINCT  TOWN, province TO_NUMBER(POPULATION), CASE WHEN HAS_LIBRARY = 'Y' THEN 1 ELSE 0 END FROM fsdb.busstops;
+SELECT DISTINCT  TOWN, province, TO_NUMBER(POPULATION), CASE WHEN HAS_LIBRARY = 'Y' THEN 1 ELSE 0 END FROM fsdb.busstops;
 
--- LIBRARIES
-INSERT INTO libraries (CIF, name, foundation_date, municipality_id, address, email, telephone)
-SELECT DISTINCT user_id, name, birthdate, town, address, email, phone from fsdb.loans where UPPER(name) LIKE '%BIBLIOTECA%';
+-- Libraries
+insert into libraries (CIF, name, foundation_date, municipality_id, address, email, telephone)
+select distinct user_id, name, TO_DATE(birthdate, 'DD.MM.YYYY'), town, address, email, phone from fsdb.loans where UPPER(name) like '%BIBLIOTECA%' AND town IN (SELECT name FROM municipalities);
 
--- BIBUS
-INSERT INTO bibuses (plate, last_itv, next_itv, state)
-SELECT plate, TO_CHAR(MAX(TO_DATE(last_itv, 'DD.MM.YYYY // HH24:MI:SS')), 'DD.MM.YYYY // HH24:MI:SS'), 
-TO_CHAR(MIN(TO_DATE(next_itv, 'DD.MM.YYYY')), 'DD.MM.YYYY') FROM fsdb.busstops GROUP BY plate;
+-- Bibus
+insert into bibuses (plate, last_itv, next_itv)
+SELECT plate, TO_DATE(MAX(TO_DATE(last_itv, 'DD.MM.YYYY // HH24:MI:SS')), 'DD.MM.YYYY // HH24:MI:SS'), 
+TO_DATE(MIN(TO_DATE(next_itv, 'DD.MM.YYYY')), 'DD.MM.YYYY') FROM fsdb.busstops GROUP BY plate;
 
--- BIBUSEROS
-INSERT INTO (passport, fullname, telephone, email, contract_start, contract_end, state)
-SELECT DISTINCT lib_passport, lib_fullname,  lib_phone, lib_email, cont_start, cont_end  FROM fsdb.busstops;
+--Bibuseros
+insert into bibuseros (passport, fullname, telephone, email, contract_start, contract_end)
+select distinct lib_passport, lib_fullname,  lib_phone, lib_email, TO_DATE(cont_start, 'DD.MM.YYYY'), TO_DATE(cont_end, 'DD.MM.YYYY')  from fsdb.busstops;
 
--- ROUTES
+--Routes
 INSERT INTO routes (id, stop_day, stop_time, municipality_id, bibus_id, bibusero_id)
-SELECT route_id, stopdate, stoptime, town, plate, lib_passport FROM fsdb.busstops ;
+SELECT route_id, TO_DATE(stopdate, 'DD.MM.YYYY'), TO_TIMESTAMP(stoptime, 'HH24:MI:SS'), town, plate, lib_passport FROM fsdb.busstops ;
